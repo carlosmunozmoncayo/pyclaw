d8c6ce1a25f88fb69945cd19b06ab5bf093ac035
Carlos Munoz Moncayo <munozmca@kl-26147.kaust.edu.sa>
2024-06-26 20:00:08 +0300

    Fix import

--- examples/acoustics_2d_variable/test_acoustics_2d_variable_io.py
+++ examples/acoustics_2d_variable/test_acoustics_2d_variable_io.py
@@ -1,15 +1,14 @@
 def test_acoustics_2d_variable_io():
     """Test I/O on variable-coefficient 2D acoustics application"""
-
+    import os
     from . import acoustics_2d_interface
+    thisdir = os.path.dirname(__file__)
 
     def verify_acoustics_io(controller):
         """ Verifies I/O on 2d variable-coefficient acoustics application"""
-        import os
         from clawpack.pyclaw.util import check_diff
         from clawpack.pyclaw import Solution
         
-        thisdir = os.path.dirname(__file__)
         verify_dir = os.path.join(thisdir,'./io_test_verification')
         
         # Expected solution
@@ -52,7 +51,7 @@ def test_acoustics_2d_variable_io():
                 return aux_err
         else:
             return
-
+    
     tempdir = os.path.join(thisdir,'_io_test_results')
     claw = acoustics_2d_interface.setup(outdir=tempdir,num_cells=(50,50))
     claw.run()
